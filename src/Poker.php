bce332ab004c5ef1d2b40d29d724937315d57c18
John Espitia <jcespitia1@gmail.com>
2020-01-13 03:20:52 -0500

    Documentación de clase

--- src/Poker.php
+++ src/Poker.php
@@ -1,18 +1,63 @@
 <?php
 
 namespace Poker;
-
+/**
+ * Clase que organiza las cartas de un juego de poker
+ * @author John Espitia <jcespitia1@gmail.com>
+ * @version 1
+ * @since 13/01/2020
+ * 
+ */
 Class Poker{
-    
+   
+    /**
+     * Atributo que almacena las cartas del juego
+     * @var array arreglo de cartas de juego de poker
+     * @example [1,2,3,5,12,10,14]
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     protected $cards;
 
+    /**
+     * Constante para definir el valor mayor del AS 
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     const ASMAX = 14;
+
+    /**
+     * Constante para definir el valor menor del AS 
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     const ASMIN = 1;
 
+    /**
+     * Constructor de la clase inicializa el atributo cards
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     public function __construct(){
         $this->cards = [];
     }
 
+    /**
+     * Método para adicionar cartas a la mano del jugador
+     * @param int $card Valor de la carta debe estar entre 1 y 14
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     public function addCard(int $card){
         if(sizeof($this->cards)<7){
             if($card>0 && $card<15){
@@ -25,13 +70,38 @@ Class Poker{
         }
     }
 
+    /**
+     * Método que retorna las cartas del jugador
+     * @return array las cartas del jugador
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     public function getCards(){
         return $this->cards;
     }
-    private function cleanCards(){
+    
+    /**
+     * Método que borra todas las cartas del jugador 
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
+    public function cleanCards(){
         $this->cards = [];
     }
 
+    /**
+     * Método que verifica si hay escalera y retorna true o false 
+     * @return boolean Retorna true si hay escalera y false si no
+     * @param array $cards Cartas de juego agregadas a la mano del jugador
+     * @author John Espitia <jcespitia1@gmail.com>
+     * @version 1
+     * @since 13/01/2020
+     * 
+     */
     public function isStraight( $cards = [] ){
         foreach($cards as $c){
             $this->addCard($c);
@@ -56,11 +126,9 @@ Class Poker{
                 }
             }
             if($consecutivas>=4) {
-                $this->cleanCards();
                 return true;
             }
         }
-        $this->cleanCards();
         return false;
     }
 
--- test/PokerTest.php
+++ test/PokerTest.php
@@ -4,17 +4,22 @@ use PHPUnit\Framework\TestCase;
 require __DIR__.'/../src/Poker.php';
 final class PokerTest extends TestCase
 {
-    private $poker;
  
     public function testAlgorithm() {
-        $this->poker = new Poker\Poker();       
-        $results1 = $this->poker->isStraight([2, 3, 4 ,5, 6]);
+        $mano1 = new Poker\Poker();       
+        $results1 = $mano1->isStraight([2, 3, 4 ,5, 6]);
         $this->assertEquals($results1, true, "2, 3, 4 ,5, 6");
-        $results2 = $this->poker->isStraight([14, 5, 4 ,2, 3]);
+        
+        $mano2 = new Poker\Poker();       
+        $results2 = $mano2->isStraight([14, 5, 4 ,2, 3]);
         $this->assertEquals($results2, true, "14, 5, 4 ,2, 3");
-        $results3 = $this->poker->isStraight([7, 7, 12 ,11, 3, 4, 14]);
+        
+        $mano3 = new Poker\Poker();       
+        $results3 = $mano3->isStraight([7, 7, 12 ,11, 3, 4, 14]);
         $this->assertEquals($results3, false, "7, 7, 12 ,11, 3, 4, 14");
-        $results4 = $this->poker->isStraight([7, 3, 2]);
+
+        $mano4 = new Poker\Poker();       
+        $results4 = $mano4->isStraight([7, 3, 2]);
         $this->assertEquals($results4, false, "7, 3, 2");
     }
         
