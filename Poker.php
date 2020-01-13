7358279a81d6eb8776cf498a086db39b12188ea0
John Espitia <jcespitia1@gmail.com>
2020-01-13 02:20:20 -0500

    Validación de escalera

--- Poker.php
+++ Poker.php
@@ -3,11 +3,22 @@ Class Poker{
     
     protected $cards;
 
+    const ASMAX = 14;
+    const ASMIN = 1;
+
+    public function __construct(){
+        $this->cards = [];
+    }
+
     public function addCard(int $card){
-        if($card>0 && $card<15){
-            $this->cards[] = $card;
+        if(sizeof($this->cards)<7){
+            if($card>0 && $card<15){
+                $this->cards[] = $card;
+            }else{
+                throw new Exception("{$card} No es una carta válida");
+            }
         }else{
-            throw new Exception("No es una carta válida");
+            throw new Exception("La mano puede tener máximo 7 cartas");
         }
     }
 
@@ -15,10 +26,32 @@ Class Poker{
         return $this->cards;
     }
 
-    public function isStraight( $cards = []){
-        if(!empty($cards) && sizeof($cards)>0){
-            $this->cards = array_combine($this->cards,$cards);
+    public function isStraight( $cards = [] ){
+        foreach($cards as $c){
+            $this->addCard($c);
         }
+        
+        if(in_array(self::ASMAX,$this->cards)) { 
+            $this->cards[]=self::ASMIN; 
+        }else if(in_array(self::ASMIN,$this->cards)) {
+            $this->cards[]=self::ASMAX; 
+        }
+
+        sort($this->cards);
+        
+        $consecutivas = 0;
+        
+        foreach($this->cards as $k => $card){
+            if($k > 0){
+                if($card-$this->cards[$k-1]==1){
+                    $consecutivas++;
+                }else{
+                    $consecutivas=0;
+                }
+            }
+            if($consecutivas>=4) return true;
+        }
+        return false;
     }
 
 }
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include 'Poker.php';
+
+$juego = new Poker();
+
+/*
+$juego->addCard(2);
+$juego->addCard(7);
+$juego->addCard(8);
+$juego->addCard(5);
+$juego->addCard(10);
+$juego->addCard(9);
+$juego->addCard(11);
+
+
+$juego->addCard(9);
+$juego->addCard(10);
+$juego->addCard(11);
+$juego->addCard(12);
+$juego->addCard(13);
+
+
+$juego->addCard(7);
+$juego->addCard(8);
+$juego->addCard(12);
+$juego->addCard(13);
+$juego->addCard(14);
+
+*/
+$juego->addCard(14);
+$juego->addCard(2);
+$juego->addCard(3);
+$juego->addCard(4);
+$juego->addCard(5);
+$juego->addCard(7);
+
+
+var_dump($juego->isStraight( ));
+
+/**
+ * escalera: 14-2-3-4-5
+ * escalera: 9-10-11-12-13
+ * escalera: 2-7-8-5-10-9-11
+ * no es escalera: 7-8-12-13-14
+ * 
+ */
